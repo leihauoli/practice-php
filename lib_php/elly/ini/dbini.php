b6fd7237644d7749bad95834485ca72daf7ead18
erika.k <e.kitada@leihauoli.com>
2014-05-13 01:18:49 +0900

    initial commit

---
+++ lib_php/elly/ini/dbini.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+/******************************************************
+/DB接続情報　create　2014/04/21　update　xxxx/xx/xx
+/
+/
+******************************************************/
+
+define ("C_DB_HOSTNAME", "localhost"); //DB no SV name
+define ("C_DB_USERNAME", "centkun");
+define ("C_DB_USERPASS", "centkun");
+define ("C_DB_NAME", "kitada"); //
+
+?>
---
+++ lib_php/elly/staff_list.php
@@ -0,0 +1,579 @@
+<?php
+
+$staff_array[] = array(
+	"id" => "000001",
+	"name" => "竹内 真",
+	"kana" => "Takeuchi Shin",
+	"post" => "代表取締役",
+	"birth_date" => "1978/07/05",
+	"blood_type" => "A（AA）"
+);
+
+$staff_array[] = array(
+	"id" => "000002",
+	"name" => "山口 彰",
+	"kana" => "Yamaguchi Akira",
+	"post" => "COO（取締役）",
+	"birth_date" => "1977/10/02",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000005",
+	"name" => "猪狩 丈治",
+	"kana" => "Takeharu Igari",
+	"post" => "エバンジェリスト、チーフフロントエンドエンジニア",
+	"birth_date" => "1982/08/17",
+	"blood_type" => "B（BO）"
+);
+
+$staff_array[] = array(
+	"id" => "000009",
+	"name" => "諸星 一行",
+	"kana" => "Morohoshi Kazuyuki",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1983/04/25",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000010",
+	"name" => "水柿 優子",
+	"kana" => "Mizugaki Yuko",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1980/7/31",
+	"blood_type" => "O(純正）"
+);
+
+$staff_array[] = array(
+	"id" => "000011",
+	"name" => "山口 俊一",
+	"kana" => "Yamaguchi Shunichi",
+	"post" => "エバンジェリスト、フロントエンドエンジニア",
+	"birth_date" => "1980/1/27",
+	"blood_type" => "B（BO）"
+);
+
+$staff_array[] = array(
+	"id" => "000013",
+	"name" => "宮本 由三佳",
+	"kana" => "Miyamoto Yumika",
+	"post" => "管理",
+	"birth_date" => "1981/01/19",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000016",
+	"name" => "北田 えりか",
+	"kana" => "Kitada Erika",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1978/10/09",
+	"blood_type" => "AB "
+);
+
+$staff_array[] = array(
+	"id" => "000017",
+	"name" => "村井　優太",
+	"kana" => "Murai Yuta",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1981/12/12",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000021",
+	"name" => "伊藤 悠子",
+	"kana" => "Ito Yuko",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1984/08/08",
+	"blood_type" => "A(AO)"
+);
+
+$staff_array[] = array(
+	"id" => "000023",
+	"name" => "有村　友秀",
+	"kana" => "Arimura　Tomohide",
+	"post" => "営業",
+	"birth_date" => "1979/09/02",
+	"blood_type" => "B型"
+);
+
+$staff_array[] = array(
+	"id" => "000024",
+	"name" => "砥柄　貴宜",
+	"kana" => "Totsuka　Takanori",
+	"post" => "エンジニア",
+	"birth_date" => "1983/09/25",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000026",
+	"name" => "山田　晃士",
+	"kana" => "Yamada  Akito",
+	"post" => "エンジニア",
+	"birth_date" => "1984/11/26",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000027",
+	"name" => "舘野　真",
+	"kana" => "Tateno　Makoto",
+	"post" => "エンジニア",
+	"birth_date" => "1984/01/05",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000028",
+	"name" => "原田　和磨",
+	"kana" => "Harada　Kazuma",
+	"post" => "アートディレクター",
+	"birth_date" => "1982/03/22",
+	"blood_type" => "AB"
+);
+
+$staff_array[] = array(
+	"id" => "000029",
+	"name" => "加藤　健",
+	"kana" => "Kato　Takeshi",
+	"post" => "エンジニア",
+	"birth_date" => "1982/12/21",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000030",
+	"name" => "國領　祐子",
+	"kana" => "Kokuryo　Yuko",
+	"post" => "デザイナー、フロントエンドエンジニア",
+	"birth_date" => "1978/7/20",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000032",
+	"name" => "茜屋　渉",
+	"kana" => "Akaneya　Wataru",
+	"post" => "デザイナー、フロントエンドエンジニア",
+	"birth_date" => "1972/10/29",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000033",
+	"name" => "山本　真由美",
+	"kana" => "Yamamoto　Mayumi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1988/10/11",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000034",
+	"name" => "橋本　裕敬",
+	"kana" => "Hashimoto　Hirotaka",
+	"post" => "デザイナー、フロントエンドエンジニア",
+	"birth_date" => "1985/05/24",
+	"blood_type" => "AB"
+);
+
+$staff_array[] = array(
+	"id" => "000035",
+	"name" => "阿部　涼平",
+	"kana" => "Abe　Ryohei",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1981/09/27",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000037",
+	"name" => "土屋　正徳",
+	"kana" => "Masanori Tsuchiya",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1978/6/3",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000038",
+	"name" => "菅間　弘嗣",
+	"kana" => "Sugama Hirotsugu",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1984/6/5",
+	"blood_type" => "AB"
+);
+
+$staff_array[] = array(
+	"id" => "000039",
+	"name" => "増崎　裕美",
+	"kana" => "Masuzaki Hiromi",
+	"post" => "デザイナー",
+	"birth_date" => "1988/7/18",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000040",
+	"name" => "小枝　優駿",
+	"kana" => "Koeda Masatoshi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1989/10/04",
+	"blood_type" => "O型"
+);
+
+$staff_array[] = array(
+	"id" => "000041",
+	"name" => "佐藤　大介",
+	"kana" => "Sato Daisuke",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987/11/24",
+	"blood_type" => "AB型"
+);
+
+$staff_array[] = array(
+	"id" => "000042",
+	"name" => "中根　悠樹",
+	"kana" => "Nakane Yuki",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1986/09/18",
+	"blood_type" => "A型"
+);
+
+$staff_array[] = array(
+	"id" => "000045",
+	"name" => "柳下　優介",
+	"kana" => "Yagishita Yusuke",
+	"post" => "エンジニア",
+	"birth_date" => "1989/02/09",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000046",
+	"name" => "堤　克靖",
+	"kana" => "Tsutsumi　Yoshiyasu",
+	"post" => "エンジニア",
+	"birth_date" => "1980/02/11",
+	"blood_type" => "B型"
+);
+
+$staff_array[] = array(
+	"id" => "000047",
+	"name" => "田中　邦典",
+	"kana" => "Tanaka　Kuninori",
+	"post" => "JavaScriptスペシャリスト・エンジニア",
+	"birth_date" => "1966/05/28",
+	"blood_type" => "O型"
+);
+
+$staff_array[] = array(
+	"id" => "000048",
+	"name" => "大滝　俊之",
+	"kana" => "Otaki Toshiyuki",
+	"post" => "デザイナー、フロントエンドエンジニア",
+	"birth_date" => "1984/09/20",
+	"blood_type" => "A型"
+);
+
+$staff_array[] = array(
+	"id" => "000049",
+	"name" => "滝野　雅俊",
+	"kana" => "Takino Masatoshi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1986/03/22",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000050",
+	"name" => "高田　裕樹",
+	"kana" => "Takada Hiroki",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1982/10/05",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000051",
+	"name" => "渡辺　真由子",
+	"kana" => "Watanabe Mayuko",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1986/02/15",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000052",
+	"name" => "甘利　大",
+	"kana" => "Yutaka Amari",
+	"post" => "デザイナー、フロントエンドエンジニア",
+	"birth_date" => "1981/06/30",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000053",
+	"name" => "米須　真由美",
+	"kana" => "Komesu Mayumi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1983/10/29",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000054",
+	"name" => "鬼澤　徹也",
+	"kana" => "Onizawa　Tetsuya",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1984/05/30",
+	"blood_type" => "?"
+);
+
+$staff_array[] = array(
+	"id" => "000055",
+	"name" => "新井　彩",
+	"kana" => "Arai　Aya",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1985/02/21",
+	"blood_type" => "AB"
+);
+
+$staff_array[] = array(
+	"id" => "000056",
+	"name" => "櫻田　拓也",
+	"kana" => "Sakurada　Takuya",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987/02/22",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000058",
+	"name" => "為田　功太郎",
+	"kana" => "",
+	"post" => "エンジニア",
+	"birth_date" => "1980/08/17",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000060",
+	"name" => "皆川　拓也",
+	"kana" => "",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1988/09/10",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000061",
+	"name" => "門屋　章子",
+	"kana" => "Kadoya Akiko",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987.10.20",
+	"blood_type" => "A型"
+);
+
+$staff_array[] = array(
+	"id" => "000062",
+	"name" => "有馬　哲平",
+	"kana" => "Arima Teppei",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1983.06.21",
+	"blood_type" => "A型"
+);
+
+$staff_array[] = array(
+	"id" => "000064",
+	"name" => "石川　健太",
+	"kana" => "Ishikawa Kenta",
+	"post" => "デザイナー",
+	"birth_date" => "1990.08.10",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000066",
+	"name" => "大竹　沙織",
+	"kana" => "Otake Saori",
+	"post" => "デザイナー",
+	"birth_date" => "1990/04/26",
+	"blood_type" => "？"
+);
+
+$staff_array[] = array(
+	"id" => "000067",
+	"name" => "渡邊　磨耶",
+	"kana" => "Watanabe Maya",
+	"post" => "デザイナー",
+	"birth_date" => "1990/07/04",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000068",
+	"name" => "山添　奈穂子",
+	"kana" => "yamazoe nahoko",
+	"post" => "デザイナー",
+	"birth_date" => "1989/07/12",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000069",
+	"name" => "大木　裕子",
+	"kana" => "Oki Yuko",
+	"post" => "デザイナー／フロントエンドエンジニア",
+	"birth_date" => "1983/6/29",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000070",
+	"name" => "田口　宗孝",
+	"kana" => "",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1984/12/21",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000071",
+	"name" => "柏﨑　巴瑠香",
+	"kana" => "",
+	"post" => "デザイナー",
+	"birth_date" => "1987/05/03",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000072",
+	"name" => "村中　亮介",
+	"kana" => "Ryosuke Muranaka",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987/11/30",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000073",
+	"name" => "増澤　仁",
+	"kana" => "Masuzawa Hitoshi",
+	"post" => "エンジニア",
+	"birth_date" => "1982/11/06",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000074",
+	"name" => "山田　和弘",
+	"kana" => "Kazuhiro Yamada",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1979/06/20",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000075",
+	"name" => "森崎　絵里奈",
+	"kana" => "Morisaki Erina",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987/07/24",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000076",
+	"name" => "木村　大智",
+	"kana" => "Kimura Daichi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1985/10/13",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000078",
+	"name" => "土肥　直樹",
+	"kana" => "Doi　Naoki",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1991/01/18",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000079",
+	"name" => "平田　雅弓",
+	"kana" => "Hirata　Masami",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1983/09/02",
+	"blood_type" => "O"
+);
+
+$staff_array[] = array(
+	"id" => "000080",
+	"name" => "秋山　なぎさ",
+	"kana" => "Akiyama　Nagisa",
+	"post" => "デザイナー／フロントエンドエンジニア",
+	"birth_date" => "1982/10/04",
+	"blood_type" => " A"
+);
+
+$staff_array[] = array(
+	"id" => "000081",
+	"name" => "森　裕輔",
+	"kana" => "Yusuke Mori",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1985/08/04",
+	"blood_type" => "B"
+);
+
+$staff_array[] = array(
+	"id" => "000083",
+	"name" => "根岸　周一",
+	"kana" => "Negishi Shuichi",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1978/01/30",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000084",
+	"name" => "笹島　大和",
+	"kana" => "",
+	"post" => "フロントエンドエンジニア",
+	"birth_date" => "1987年1月11日",
+	"blood_type" => "B型"
+);
+
+$staff_array[] = array(
+	"id" => "000085",
+	"name" => "藤中　亮太",
+	"kana" => "Fujinaka Ryota",
+	"post" => "アシスタントデザイナー",
+	"birth_date" => "1991年5月13日",
+	"blood_type" => "A型"
+);
+
+$staff_array[] = array(
+	"id" => "000086",
+	"name" => "丸岡　咲希",
+	"kana" => "Maruoka Saki",
+	"post" => "アシスタントデザイナー",
+	"birth_date" => "1991/04/12",
+	"blood_type" => "A"
+);
+
+$staff_array[] = array(
+	"id" => "000087",
+	"name" => "牧田　直也",
+	"kana" => "",
+	"post" => "ディレクター",
+	"birth_date" => "",
+	"blood_type" => ""
+);
+
+?>
